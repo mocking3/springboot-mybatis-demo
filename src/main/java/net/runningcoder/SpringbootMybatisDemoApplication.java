8778c99f2104387178a6f7afd3d547f6083f8ffb
moc <758548361@qq.com>
2017-12-17 13:09:39 +0800

    Initial commit

---
+++ src/main/java/net/runningcoder/SpringbootMybatisDemoApplication.java
@@ -0,0 +1,12 @@
+package net.runningcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatisDemoApplication.class, args);
+	}
+}
